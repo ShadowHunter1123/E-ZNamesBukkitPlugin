2d6c1cdd2c63199b44a36b06a20c079883546c0b
ShadowHunter1123 <awilliams0428@hotmail.com>
2014-11-06 09:59:51 -0600

    More cleanup

--- EasyNames/src/com/shadow/eznames/EasyNames.java
+++ EasyNames/src/com/shadow/eznames/EasyNames.java
@@ -2,17 +2,22 @@ package com.shadow.eznames;
 
 import java.io.File;
 
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.shadow.eznames.commands.EasyCommands;
 
-public class EasyNames extends JavaPlugin{
+public class EasyNames extends JavaPlugin implements Listener{
 
 	@Override
 	public void onEnable(){
 		
 		getCommand("ez").setExecutor(new EasyCommands());
-		
+		getServer().getPluginManager().registerEvents(this, this);
 		System.out.println("EasyNames beta enabled!");
 		File config = new File(getDataFolder(), File.separator + "config.yml");
 		
@@ -26,6 +31,11 @@ public class EasyNames extends JavaPlugin{
 	@Override
 	public void onDisable(){
 		System.out.println("EasyNames disabled");
+	}
+	@EventHandler
+	public void joinEvent(PlayerJoinEvent e){
+		Player player = e.getPlayer();
+		player.sendMessage(ChatColor.GREEN + "Hey you! Do /ez help to view E-Z Names commands!");
 	}
 	
 }
